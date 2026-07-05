63858a240519607fe2b9e9ae5acc6b99458a93a1
Yash Sirohi <ysirohi773@gmail.com>
2026-07-05 20:19:35 +0530

    Learned Pointer Basics in C++

---
+++ 75_Pointers_Basics.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main ()
+{
+    int a = 10;
+
+    int *p = &a;
+
+    cout << "Value of a : " << a << endl;
+    cout << "Address of a : " << &a << endl;
+    cout << "Value stored in p : " << p << endl;
+    cout << "Value at address stored in p : " << *p << endl;
+
+    
+    return 0;
+}
